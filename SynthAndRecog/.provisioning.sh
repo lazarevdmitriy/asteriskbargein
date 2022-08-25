39b784d97560b6a54acdc2ba7df6f0f5ea0b836b
dmitriy.rubbert@gmail.com <dmitriy.rubbert@gmail.com>
2022-08-25 17:24:11 +0300

    Update provisionins. Set options (voice,pitch,volume,volume) from SpeechRecognitionConnector

--- SynthAndRecog/.provisioning.sh
+++ SynthAndRecog/.provisioning.sh
@@ -4,17 +4,19 @@ user=`echo $credentials | jq '.user' | cut -d'"' -f2`
 pass=`echo $credentials | jq '.pass' | cut -d'"' -f2`
 base=`echo $credentials | jq '.base' | cut -d'"' -f2`
 host=`echo $credentials | jq '.host' | cut -d'"' -f2`
+cd "$(dirname "$0")"
 
 # export TTS_SERVER=10.3.0.9
 # export QA_SERVER=10.3.0.19
-envsubst < ./SynthAndRecog/examples/mrcpconnectors.conf > /etc/asterisk/mrcpconnectors.conf
-envsubst < ./SynthAndRecog/examples/tts.xml > /usr/local/unimrcp/conf/client-profiles/tts.xml
+
+envsubst < examples/mrcpconnectors.conf > /etc/asterisk/mrcpconnectors.conf
+envsubst < examples/tts.xml > /usr/local/unimrcp/conf/client-profiles/tts.xml
 
 mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, '&i=any','') WHERE type = 'SpeechRecognitionConnector';"
 mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, 'b=1&','') WHERE type = 'SpeechRecognitionConnector';"
 
 mysql -u $user --password=$pass -h $host $base -e  "DROP FUNCTION IF EXISTS _phoneGetIVR;"
-zcat ./SynthAndRecog/_phoneGetIVR.sql.gz | mysql -u $user --password=$pass -h $host $base
+zcat _phoneGetIVR.sql.gz | mysql -u $user --password=$pass -h $host $base
 
 mysql -u $user --password=$pass -h $host $base -e  "update updated set is_updated=2;"
 firewall-cmd --zone=public --permanent --add-port=38000-39000/udp --add-port=8062/tcp && firewall-cmd --reload
