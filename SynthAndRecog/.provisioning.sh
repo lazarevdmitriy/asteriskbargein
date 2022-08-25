0af06ce828743ab196aa96a42be8d95941ac881e
Dmitriy Lazarev <lazarev.d@lexicom.ai>
2022-08-25 14:13:45 +0000

    Update SynthAndRecog/.provisioning.sh

--- SynthAndRecog/.provisioning.sh
+++ SynthAndRecog/.provisioning.sh
@@ -5,12 +5,16 @@ pass=`echo $credentials | jq '.pass' | cut -d'"' -f2`
 base=`echo $credentials | jq '.base' | cut -d'"' -f2`
 host=`echo $credentials | jq '.host' | cut -d'"' -f2`
 
-# server_ip=10.3.0.9
-# client_ip=10.3.0.19
+# export TTS_SERVER=10.3.0.9
+# export QA_SERVER=10.3.0.19
+envsubst < ./SynthAndRecog/examples/mrcpconnectors.conf > /etc/asterisk/mrcpconnectors.conf
+envsubst < ./SynthAndRecog/examples/tts.xml > /usr/local/unimrcp/conf/client-profiles/tts.xml
+
 mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, '&i=any','') WHERE type = 'SpeechRecognitionConnector';"
 mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, 'b=1&','') WHERE type = 'SpeechRecognitionConnector';"
-envsubst < examples/mrcpconnectors.conf > /etc/asterisk/mrcpconnectors.conf
-envsubst < examples/tts.xml > /usr/local/unimrcp/conf/client-profiles/tts.xml
-zcat _phoneGetIVR.sql | mysql -u $user --password=$pass -h $host $base
+
+mysql -u $user --password=$pass -h $host $base -e  "DROP FUNCTION IF EXISTS _phoneGetIVR;"
+zcat ./SynthAndRecog/_phoneGetIVR.sql.gz | mysql -u $user --password=$pass -h $host $base
+
 mysql -u $user --password=$pass -h $host $base -e  "update updated set is_updated=2;"
 firewall-cmd --zone=public --permanent --add-port=38000-39000/udp --add-port=8062/tcp && firewall-cmd --reload
