314fb01ccff695a90c80f585a9d3819c48f0ecf4
dmitriy.rubbert@gmail.com <dmitriy.rubbert@gmail.com>
2022-08-25 16:44:26 +0300

    Update provisioning.sh

--- SynthAndRecog/.provisioning.sh
+++ SynthAndRecog/.provisioning.sh
@@ -7,10 +7,10 @@ host=`echo $credentials | jq '.host' | cut -d'"' -f2`
 
 # server_ip=10.3.0.9
 # client_ip=10.3.0.19
-mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, '&i=any','') WHERE `type` = 'SpeechRecognitionConnector';"
-mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, 'b=1&','') WHERE `type` = 'SpeechRecognitionConnector';"
-envsubst << examples/mrcpconnectors.conf >> /etc/asterisk/mrcpconnectors.conf
-envsubst << examples/tts.xml >> /usr/local/unimrcp/conf/client-profiles/tts.xml
+mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, '&i=any','') WHERE type = 'SpeechRecognitionConnector';"
+mysql -u $user --password=$pass -h $host $base -e  "UPDATE connectors SET param = REPLACE(param, 'b=1&','') WHERE type = 'SpeechRecognitionConnector';"
+envsubst < examples/mrcpconnectors.conf > /etc/asterisk/mrcpconnectors.conf
+envsubst < examples/tts.xml > /usr/local/unimrcp/conf/client-profiles/tts.xml
 zcat _phoneGetIVR.sql | mysql -u $user --password=$pass -h $host $base
 mysql -u $user --password=$pass -h $host $base -e  "update updated set is_updated=2;"
 firewall-cmd --zone=public --permanent --add-port=38000-39000/udp --add-port=8062/tcp && firewall-cmd --reload
